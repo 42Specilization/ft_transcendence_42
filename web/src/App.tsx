f566432b648665cf9fb6e9a93ff770a0c41fbe4c
MarcusVinix <vi.ni159marcus@gmail.com>
2022-09-20 10:07:18 -0300

    Creating page folder

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,15 @@
 import './styles/main.css';
-import pongGame from './assets/pong-game.png'
+import { SigIn } from './pages/SigIn'
+import { Home } from './pages/Home'
+import { useRef, useState } from 'react';
 
 function App() {
 
+  const [sigInVar, setSigInVar] = useState<boolean>(false)
+
   return (
-    <div className='max-w-[1344px] mx-auto my-20 flex flex-col items-center'>
-      {/* <h1 className='text-9xl font-extrabold text-white text-shadow-style my-28'>Pong Game</h1> */}
-      <img src={pongGame} alt='title' className='my-28' />
-      <button className='text-5xl text-white bg-violet-500 px-4 py-3 h-32 w-[600px] rounded-[100px] font-bold border-4 border-white hover:bg-violet-600'>Sign in</button>
+    <div>
+      {sigInVar ? <Home /> : <SigIn sigIn={setSigInVar} />}
     </div>
   )
 }
---
+++ web/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+
+
+export function Home() {
+	return (
+		<div>
+			<h1>Hello i am home!</h1>
+		</div>
+	)
+}
