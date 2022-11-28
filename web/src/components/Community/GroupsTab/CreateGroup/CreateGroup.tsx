6fbe86f398c8b795baa2a570ef6f7b0c0b778ef8
gsilva-v <gaa.silva53@gmail.com>
2022-11-28 15:45:42 -0300

    Fix new envs

--- web/src/components/Community/GroupsTab/CreateGroup/CreateGroup.tsx
+++ web/src/components/Community/GroupsTab/CreateGroup/CreateGroup.tsx
@@ -1,4 +1,4 @@
-import'./CreateGroup.scss';
+import './CreateGroup.scss';
 import { Dispatch, SetStateAction, useContext, useEffect, useState } from 'react';
 import { actionsStatus } from '../../../../adapters/status/statusState';
 import { IntraDataContext } from '../../../../contexts/IntraDataContext';
@@ -6,20 +6,19 @@ import { Dropzone } from '../../../Profile/UserImage/Dropzone';
 import { CheckSquare, Image } from 'phosphor-react';
 import { Group } from '../../../../../../api/src/chat/entities/group.entity';
 import { CreateGroupData } from '../../../../others/Interfaces/interfaces';
-import { Checkbox } from '../../../Game/Checkbox/Checkbox';
-import { Message } from '../../../../../../api/src/chat/entities/message.entity';
+import { Checkbox } from '../../../Checkbox/Checkbox';
 interface CreateGroupProps {
   setCreateGroupModal: Dispatch<SetStateAction<boolean>>
 }
 
-export function CreateGroup({setCreateGroupModal}: CreateGroupProps) {
-  const { api, config, intraData} = useContext(IntraDataContext);
+export function CreateGroup({ setCreateGroupModal }: CreateGroupProps) {
+  const { api, config, intraData } = useContext(IntraDataContext);
   const [selectedFile, setSelectedFile] = useState<File>();
   const [privateGroup, setPrivateGroup] = useState<boolean>(false);
   const [placeHolder, setPlaceHolder] = useState('Group Name');
   const [password, setPassword] = useState('Password');
 
-  async function handleSubmit(groupData: CreateGroupData ) {
+  async function handleSubmit(groupData: CreateGroupData) {
     const data = new FormData();
     data.append('name', 'chatImage');
     if (selectedFile) {
@@ -32,23 +31,23 @@ export function CreateGroup({setCreateGroupModal}: CreateGroupProps) {
     }
     console.log(groupData);
     await api.post('/chat/createGroup', groupData, config)
-      .catch((err)=>{
+      .catch((err) => {
         // console.log(err.response.data.message);
         setPassword(err.response.data.message);
       });
-      
+
     // actionsStatus.changeImage(selectedFile?.name);
   }
-    
-  function handleKeyEnter(event:any) {
+
+  function handleKeyEnter(event: any) {
     event.preventDefault();
-    if (event.target[0].value === ''){
+    if (event.target[0].value === '') {
       setPlaceHolder('Group need have a name');
-      return ;
+      return;
     }
-   
-    let type = event.target[1].value !== '' ? 'protected' :'public';
-    if(event.target[3].ariaChecked === 'true'){
+
+    let type = event.target[1].value !== '' ? 'protected' : 'public';
+    if (event.target[3].ariaChecked === 'true') {
       type = 'private';
       event.target[1].value = '';
       event.target[2].value = '';
@@ -76,29 +75,29 @@ export function CreateGroup({setCreateGroupModal}: CreateGroupProps) {
       </div>
       <form autoComplete='off' className='createGroup__form' action="submit" onSubmit={handleKeyEnter}>
         <input
-          className='createGroup__groupName' 
+          className='createGroup__groupName'
           type="text"
           name="groupName"
           placeholder={placeHolder}
           style={{ border: placeHolder !== 'Group Name' ? '3px solid red' : 'none' }
-          }/>
+          } />
         <input
-          className='createGroup__groupPassword' 
+          className='createGroup__groupPassword'
           type="text"
-          name="groupPassword" 
+          name="groupPassword"
           placeholder={password}
           style={{ border: password !== 'Password' ? '3px solid red' : 'none' }}
         />
         <input
           className='createGroup__groupPassword'
           type="text"
-          name="groupPassword" 
+          name="groupPassword"
           placeholder='Confirm Password'
         />
         <label className='createGroup__private' htmlFor="">
-          <span>Private Group</span>   
-          <Checkbox 
-            id='privateGroup' 
+          <span>Private Group</span>
+          <Checkbox
+            id='privateGroup'
             onCheckedChange={() => setPrivateGroup(!privateGroup)}
           />
         </label>
