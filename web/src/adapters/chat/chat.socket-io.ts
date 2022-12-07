2b0ba0c8ca99bbb9247b4eab1ead9b32e97300a8
MarcusVinix <vi.ni159marcus@gmail.com>
2022-12-07 17:27:08 -0300

    fix eslint

--- web/src/adapters/chat/chat.socket-io.ts
+++ web/src/adapters/chat/chat.socket-io.ts
@@ -1,10 +1,11 @@
+/* eslint-disable indent */
 import { io, Socket } from 'socket.io-client';
 import { MsgToClient } from '../../others/Interfaces/interfaces';
 import { actionsStatus } from '../status/statusState';
 import { AppActionsChat, AppStateChat } from './chatState';
 
 export const socketChatIOUrl = `http://${import.meta.env.VITE_API_HOST}:${import.meta.env.VITE_API_PORT
-}/${import.meta.env.VITE_CHAT_NAMESPACE}`;
+  }/${import.meta.env.VITE_CHAT_NAMESPACE}`;
 
 export interface CreateSocketChatOptions {
   accessToken?: string | undefined | null;
